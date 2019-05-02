8756bbf123d6b24124e222c4423ab56d9abaae40
Fredrick Jones <fredrick.w.jones@gmail.com>
2019-05-02 14:37:17 -0600

    adding 'likes' functionality

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -10,6 +10,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 // ORDERING IN THIS LIST OF ROUTES IS IMPORTANT;
 export const appRoutes: Routes = [
@@ -19,11 +20,11 @@ export const appRoutes: Routes = [
 		runGuardsAndResolvers: 'always',
 		canActivate: [AuthGuard],
 		children: [
-			{ path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
-			{ path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
-			{ path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] },
+			{ path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+			{ path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+			{ path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
 			{ path: 'messages', component: MessagesComponent },
-			{ path: 'lists', component: ListsComponent }
+			{ path: 'lists', component: ListsComponent, resolve: {users: ListsResolver}}
 		]
 	},
 	{ path: '**', redirectTo: '', pathMatch: 'full' }
